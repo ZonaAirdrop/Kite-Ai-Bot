6bd1aec0230a58e24f6db4f07e0db1dc6e2e92bb
Zona Airdrop <80012026+ZonaAirdrop@users.noreply.github.com>
2025-07-09 01:22:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const logger = {
   banner: () => {
     console.log(`${colors.cyan}${colors.bold}`);
     console.log('---------------------------------------------');
-    console.log('     Kite AI Automation Bot - YetiDAO ');
+    console.log('     Kite AI Bot - Zona Airdrop ');
     console.log(`---------------------------------------------${colors.reset}\n`);
   },
   agent: (msg) => console.log(`${colors.white}${msg}${colors.reset}`)
